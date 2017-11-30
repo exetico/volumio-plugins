7164e2629748c2f9e653d5f48941086cff861f0f
exetico <K5eTeYWUSak1RKji>
2017-11-30 12:41:27 +0100

    adding "Remove service"

--- plugins/music_service/podstream/uninstall.sh
+++ plugins/music_service/podstream/uninstall.sh
@@ -3,6 +3,8 @@
 echo "Uninstalling podstream dependencies"
 sudo rm -Rf /data/configuration/music_service/podstream/
 sudo rm -Rf /data/plugins/music_service/podstream/
+sudo rm -Rf /data/plugins/music_service/podstream/
+sudo rm -f /etc/systemd/system/podstream.service
 echo "Removing podstream"
 echo " Removing podstream configuration file"
 
